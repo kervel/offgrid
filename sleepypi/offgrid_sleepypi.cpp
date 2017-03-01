70fc05df284471dcbeff17edd1d1ac2772d89448
Frank Dekervel <frank@kapernikov.com>
2017-03-01 23:54:47 +0100

    complete protocol impl

--- sleepypi/offgrid_sleepypi.cpp
+++ sleepypi/offgrid_sleepypi.cpp
@@ -4,6 +4,8 @@
 #include <Wire.h>
 #include <SleepyPi2.h>
 
+#define DEBUG
+
 #define SLAVE_ADDRESS 0x36
 #define MAX_SENT_BYTES 5
 
@@ -19,6 +21,10 @@ void setup()
 	Wire.begin(SLAVE_ADDRESS);
 	Wire.onRequest(request_event);
 	Wire.onReceive(receive_event);
+#ifdef DEBUG
+	Serial.begin(9600);
+	Serial.println("start");
+#endif
 }
 
 // The loop function is called in an endless loop
@@ -34,7 +40,7 @@ void loop()
 
 void request_event()
 {
-	Wire.write(regmap.regMapTemp,REGMAP_SIZE);
+	Wire.write(regmap.regMapTemp + receivedCommands[0],REGMAP_SIZE);
 }
 
 void receive_event(int bytesReceived) {
@@ -50,6 +56,30 @@ void receive_event(int bytesReceived) {
 		}
 	}
 
+	if(bytesReceived == 1 && (receivedCommands[0] < REGMAP_SIZE))
+	{
+		return;
+	}
+	if(bytesReceived == 1 && (receivedCommands[0] >= REGMAP_SIZE))
+	{
+		receivedCommands[0] = 0x00;
+		return;
+	}
+#ifdef DEBUG
+	Serial.print("Got ");
+	Serial.print(bytesReceived);
+	Serial.print(" bytes ");
+	Serial.print(receivedCommands[0]);
+	Serial.println("");
+#endif
+	for (int i=1; i<bytesReceived; i++) {
+		byte dataByte = receivedCommands[i];
+		int offset = receivedCommands[0] + (i-1);
+		if (offset < REGMAP_SIZE) {
+			regmap.regMapTemp[offset] = dataByte;
+		}
+	}
+
 }
 
 void alarm_isr()
@@ -61,27 +91,33 @@ void execute_command() {
 	unsigned char cmd = regmap.vars.command;
 	regmap.vars.command = CMD_NOTHING;
 	switch (cmd) {
-		case CMD_WAIT_ALARM : wait_alarm();
-							 break;
-		case CMD_POWEROFF_EXT : SleepyPi.enableExtPower(false);
-							 break;
-		case CMD_POWERON_EXT : SleepyPi.enableExtPower(true);
-							 break;
-		case CMD_POWEROFF_RPI : SleepyPi.startPiShutdown();
-							 break;
+	case CMD_WAIT_ALARM : wait_alarm();
+	break;
+	case CMD_POWEROFF_EXT : SleepyPi.enableExtPower(false);
+	break;
+	case CMD_POWERON_EXT : SleepyPi.enableExtPower(true);
+	break;
+	case CMD_POWEROFF_RPI : SleepyPi.startPiShutdown();
+	break;
 
 	}
 
 }
 
 void wait_alarm() {
-    attachInterrupt(0, alarm_isr, FALLING);		// Alarm pin
-    SleepyPi.enableWakeupAlarm(true);
+#ifdef DEBUG
+	Serial.println("waiting for alarm...");
+#endif
+	attachInterrupt(0, alarm_isr, FALLING);		// Alarm pin
+	SleepyPi.enableWakeupAlarm(true);
 	SleepyPi.setAlarm(regmap.vars.wakeupAlarmHour, regmap.vars.wakeupAlarmMinute);
-    delay(500);
-    SleepyPi.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-    detachInterrupt(0);
-    SleepyPi.ackAlarm();
+	delay(500);
+	SleepyPi.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+#ifdef DEBUG
+	Serial.println("got alarm...");
+#endif
+	detachInterrupt(0);
+	SleepyPi.ackAlarm();
 }
 
 
