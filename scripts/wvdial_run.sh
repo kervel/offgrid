4050481acff3c88530a4331034d571c6dfd20885
Frank Dekervel <kervel@gmail.com>
2017-03-29 22:01:47 +0200

    runner

---
+++ scripts/wvdial_run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+while sleep 15
+	do 
+		if /usr/bin/lsusb | grep  1c9e:f000; then
+			/usr/sbin/usb_modeswitch -c /etc/usb_modeswitch.conf
+		fi
+		/usr/bin/wvdial 3gconnect
+	done
+
