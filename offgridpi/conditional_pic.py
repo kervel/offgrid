fab5be40ba6b3e2b77f56089c38ce2858cefdc38
Frank Dekervel <frank@kapernikov.com>
2017-04-17 19:59:43 +0200

    script to make pic once per hour

---
+++ offgridpi/conditional_pic.py
@@ -0,0 +1,38 @@
+import datetime
+import os, os.path
+
+target_dir = "/home/pi/photos"
+checktime="mtime"
+
+
+def get_age(filename):
+    if checktime == 'mtime':
+        return datetime.datetime.fromtimestamp(os.path.getmtime(filename))
+    else:
+        return datetime.datetime.fromtimestamp(os.path.getctime(filename))
+
+def most_recent_time(dir):
+    fs = os.listdir(dir)
+    ages = [get_age(x) for x in fs]
+    if len(ages) == 0:
+        return datetime.datetime(year=1900,month=1,day=1)
+    return max(ages)
+
+def should_make_photo():
+    mrt = most_recent_time(target_dir)
+    now = datetime.datetime.now()
+    delta = now - mrt
+    if now.hour >= 23 or now.hour < 6:
+        return False
+
+    if (delta > datetime.timedelta(hours=1)):
+        return True
+    return False
+
+def make_photo():
+    now = datetime.datetime.now()
+    fname = 'still-' + now.strftime('%y%m%d-%H%M%S') + '.jpg'
+    os.system('raspistill -o /home/pi/photos/%s' % fname)
+
+if (should_make_photo()):
+    make_photo()
